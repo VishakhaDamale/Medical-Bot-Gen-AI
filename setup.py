faac94f24706519ae9dce548891030e16124baee
Vishakha <vishakadamale@gmail.com>
2024-12-11 23:10:08 +0530

    folder structure added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Vishakha Damale',
-    author_email= 'vishakhadamale369@gmail.com',
+    author= 'VishakhaDamale',
+    author_email= 'vishakadamale@gmail.com',
     packages= find_packages(),
     install_requires = []
 
